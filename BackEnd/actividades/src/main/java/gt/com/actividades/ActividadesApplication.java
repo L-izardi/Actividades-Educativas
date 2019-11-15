4a0830ba01cbfd9de1981198adac07bd3e34a1f7
L-izardi <43021652+L-izardi@users.noreply.github.com>
2019-11-15 16:35:14 -0600

    .

--- BackEnd/actividades/src/main/java/gt/com/actividades/ActividadesApplication.java
+++ BackEnd/actividades/src/main/java/gt/com/actividades/ActividadesApplication.java
@@ -2,21 +2,13 @@ package gt.com.actividades;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import gt.com.actividades.utils.AppProperty;
-
-@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
-@EnableConfigurationProperties({AppProperty.class})
-public class ActividadesApplication {
+@SpringBootApplication
+public class ActividadesApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActividadesApplication.class, args);
 	}
 
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ActividadesApplication.class);
-	}
 }
