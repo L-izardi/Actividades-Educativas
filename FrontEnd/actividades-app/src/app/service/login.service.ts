1dedfa50a8caf28d1257e19543a3d1e58f82c6d7
L-izardi <43021652+L-izardi@users.noreply.github.com>
2019-11-21 17:51:51 -0600

    Update login.service.ts

--- FrontEnd/actividades-app/src/app/service/login.service.ts
+++ FrontEnd/actividades-app/src/app/service/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpHeaders, HttpBackend } from '@angular/common/http'
 import { User } from '../model/user';
 import { Config } from './config';
 
@@ -13,13 +13,25 @@ export class LoginService {
   private http: HttpClient;
 
   constructor(
-    http: HttpClient
+    handler: HttpBackend
   ) { 
-    this.http = http;
+    this.http = new HttpClient(handler);
     this.url = Config.apiUrl;
   }
 
   login(user: User) {
-    return this.http.post(this.url + 'user/login', user);
+    console.log(user);
+    return this.http.post(this.url + 'auth/login', user);
+  }
+  loginOauth2(code){
+    const headers = new HttpHeaders({'Content-Type':'application/json; charset=utf-8'});
+    return this.http.post("https://oauth2.googleapis.com/token", {
+      "code" : code,
+      "client_id" : "124840980704-cpper58q9r06nud9ctl2kp4iu335cg1u.apps.googleusercontent.com",
+      "client_secret" : "pJRB1nhqVhnwwdQpqL3eCXNA",
+      "redirect_uri" : "http://localhost:4200/login",
+      "grant_type" : "authorization_code"
+      
+    },{headers:headers});
   }
 }
