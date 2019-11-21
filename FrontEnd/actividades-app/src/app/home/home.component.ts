8a4d811198eb9eff4917a3819956f575c6280832
L-izardi <43021652+L-izardi@users.noreply.github.com>
2019-11-21 17:51:41 -0600

    Update home.component.ts

--- FrontEnd/actividades-app/src/app/home/home.component.ts
+++ FrontEnd/actividades-app/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../service/storage.service';
+import { HttpClient, HttpBackend } from '@angular/common/http'
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-root',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
@@ -11,24 +12,40 @@ export class HomeComponent implements OnInit {
 
   private router: Router;
   private storageService: StorageService;
+  private http: HttpClient;
+  private userClient;
+  private userImage;
 
   constructor(
     storageService: StorageService,
-    router: Router
+    router: Router,
+    handler: HttpBackend
   ) { 
     this.storageService = storageService;
     this.router = router;
+    this.http = new HttpClient(handler);
+    this.userClient = null;
   }
 
   ngOnInit() {
+    if(!this.storageService.isAuthenticated()){
+      this.router.navigate(['/login']);
+    }else{
+      this.userName(this.storageService.getCurrentSession().token);
+    }
   }
 
   logout() {
     this.storageService.logout();
   }
 
-  goToProviders() {
-    this.router.navigate(['/provider']);
+  userName (id_token) {
+    var uri = 'https://oauth2.googleapis.com/tokeninfo?id_token=' + id_token;
+    return this.http.get(uri).subscribe (
+      Response => {
+        this.userClient = Response['name'];
+        this.userImage = Response['picture'];
+      });
   }
 
 }
