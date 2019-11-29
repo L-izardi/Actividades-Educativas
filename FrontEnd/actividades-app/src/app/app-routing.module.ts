914f31f8d33d25b6adc71966968146625251e3e8
L-izardi <43021652+L-izardi@users.noreply.github.com>
2019-11-28 22:19:18 -0600

    Update app-routing.module.ts

--- FrontEnd/actividades-app/src/app/app-routing.module.ts
+++ FrontEnd/actividades-app/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AuthorizatedGuard } from './guard/authorizated.guard';
-
-
+import { TcCentroComponent } from './catalog/tcCentro/tc-centro/tc-centro.component';
+import { TcCentroAddComponent } from './catalog/tcCentro/tc-centro-add/tc-centro-add.component';
+import { TcFacultadComponent } from './catalog/tcFacultad/tc-facultad/tc-facultad.component';
+import { SignUpComponent } from 'src/app/sign-up/sign-up.component';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
-  { path: 'home', component: HomeComponent, canActivate: [ AuthorizatedGuard ]}
+  { path: 'registro', component: SignUpComponent},
+  { path: 'home', component: HomeComponent, canActivate: [ AuthorizatedGuard ]},
+  { path: 'centro', component:TcCentroComponent, canActivate: [ AuthorizatedGuard ]},
+  { path: 'centro/add', component:TcCentroAddComponent, canActivate: [ AuthorizatedGuard ]},
+  { path: 'facultad', component:TcFacultadComponent, canActivate: [ AuthorizatedGuard ]}
 ];
 
 @NgModule({
