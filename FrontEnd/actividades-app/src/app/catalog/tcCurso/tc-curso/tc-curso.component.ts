ba90941164392240382de9a1b48b0edb440e7be9
L-izardi <43021652+L-izardi@users.noreply.github.com>
2019-11-28 22:21:18 -0600

    Create tc-curso.component.ts

---
+++ FrontEnd/actividades-app/src/app/catalog/tcCurso/tc-curso/tc-curso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tc-curso',
+  templateUrl: './tc-curso.component.html',
+  styleUrls: ['./tc-curso.component.scss']
+})
+export class TcCursoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
