86d14949d4d8c2ad85adf4d12061a7117fd25372
L-izardi <43021652+L-izardi@users.noreply.github.com>
2019-11-28 22:21:57 -0600

    create navbar

---
+++ FrontEnd/actividades-app/src/app/catalog/template/navbar/navbar.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/service/storage.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  
+  private storageService: StorageService;
+  private router: Router;
+  navbarOpen = false;
+
+  constructor(storageService: StorageService, router: Router) {
+    this.storageService = storageService;
+    this.router = router;
+   }
+
+  ngOnInit() {
+  }
+  
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+  
+  logout() {
+    this.storageService.logout();
+  }
+  goToHome(){
+    this.router.navigate(['/home']);
+  }
+
+  goToCentro(){
+    this.router.navigate(['/centro']);
+  }
+  goToCentroAdd(){
+    this.router.navigate(['/centro/add']);
+  }
+
+
+  goToFacultad(){
+    this.router.navigate(['/facultad']);
+  }
+}
