63f77c32defdd5221d32535c36a7a57913f9e110
L-izardi <43021652+L-izardi@users.noreply.github.com>
2019-11-30 03:27:05 -0600

    Update tc-headers.ts

--- FrontEnd/actividades-app/src/app/model/tc-headers.ts
+++ FrontEnd/actividades-app/src/app/model/tc-headers.ts
@@ -5,9 +5,9 @@ import { TcCentro } from './tc-centro';
 export class TcHeaders {
     
     public idCorrelativo:number;
-    public idCentro:TcCentro;
-    public idCurso:TcCurso;
-    public idCatedratico:TcCatedratico;
+    public tc_centro:TcCentro;
+    public tc_curso:TcCurso;
+    public tc_catedratico:TcCatedratico;
     public semestre:number;
     public seccion:String;
     public horario:String;
