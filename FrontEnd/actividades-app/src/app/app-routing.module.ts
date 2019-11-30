08bd2394be2924da17f5e223b8ab0169ef8b042d
L-izardi <43021652+L-izardi@users.noreply.github.com>
2019-11-30 03:26:49 -0600

    Update app-routing.module.ts

--- FrontEnd/actividades-app/src/app/app-routing.module.ts
+++ FrontEnd/actividades-app/src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ import { TcCentroComponent } from './catalog/tcCentro/tc-centro/tc-centro.compon
 import { TcCentroAddComponent } from './catalog/tcCentro/tc-centro-add/tc-centro-add.component';
 import { TcFacultadComponent } from './catalog/tcFacultad/tc-facultad/tc-facultad.component';
 import { SignUpComponent } from 'src/app/sign-up/sign-up.component';
+import { TcFacultadAddComponent } from './catalog/tcFacultad/tc-facultad-add/tc-facultad-add.component';
+import { TcCursoComponent } from './catalog/tcCurso/tc-curso/tc-curso.component';
+import { TcTemaComponent } from './catalog/tcCurso/tcActividad/tc-tema/tc-tema.component';
+import { TcDetalleComponent } from './catalog/tcCurso/tcActividad/tc-detalle/tc-detalle.component';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
@@ -14,7 +18,11 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [ AuthorizatedGuard ]},
   { path: 'centro', component:TcCentroComponent, canActivate: [ AuthorizatedGuard ]},
   { path: 'centro/add', component:TcCentroAddComponent, canActivate: [ AuthorizatedGuard ]},
-  { path: 'facultad', component:TcFacultadComponent, canActivate: [ AuthorizatedGuard ]}
+  { path: 'facultad', component:TcFacultadComponent, canActivate: [ AuthorizatedGuard ]},
+  { path: 'facultad/add', component:TcFacultadAddComponent, canActivate: [ AuthorizatedGuard ]},
+  { path: 'cursos', component:TcCursoComponent, canActivate: [ AuthorizatedGuard ]},
+  { path: 'curso/:idCorrelativo/actividad', component:TcTemaComponent, canActivate: [ AuthorizatedGuard ]},
+  { path: 'tema/:idTema/actividad', component:TcDetalleComponent, canActivate: [ AuthorizatedGuard ]}
 ];
 
 @NgModule({
